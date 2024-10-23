4399529ad90674655bcba90daef0940dc2b54a4c
reichikku <bladeryz@gmail.com>
2024-10-24 00:20:26 +0300

    Renamed planetarium_api folder to planetarium_service, also implemented a new app planetarium

--- planetarium_service/urls.py
+++ planetarium_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for planetarium_api project.
+URL configuration for planetarium_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
