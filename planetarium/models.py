93ffa6cca13827be562aecd76799808c94476ce6
reichikku <bladeryz@gmail.com>
2024-10-24 00:34:04 +0300

    Implemented models for the project - AstronomyShow, ShowTheme, PlanetariumDome, ShowSession, Reservation, and Ticket

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+from planetarium_service import settings
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
