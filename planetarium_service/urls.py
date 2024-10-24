7a7af16a619330d3373df265c852b324fb88b983
reichikku <bladeryz@gmail.com>
2024-10-24 16:28:43 +0300

    Fixed up main urls

--- planetarium_service/urls.py
+++ planetarium_service/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
-        "api/planetarium",
+        "api/planetarium/",
         include("planetarium.urls", namespace="planetarium")
     ),
 ]
