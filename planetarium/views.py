85bbeca2bd00e295e9a7d50adb81b537cfb9d268
reichikku <bladeryz@gmail.com>
2024-10-24 16:19:08 +0300

    Implemented basic viewsets

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from planetarium.models import (
+    AstronomyShow,
+    PlanetariumDome,
+    Reservation,
+    ShowSession,
+    ShowTheme,
+    Ticket,
+)
+from planetarium.serializers import (
+    AstronomyShowSerializer,
+    PlanetariumDomeSerializer,
+    ReservationSerializer,
+    ShowSessionSerializer,
+    ShowThemeSerializer,
+    TicketSerializer,
+)
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.all()
+    serializer_class = AstronomyShowSerializer
+
+
+class PlanetariumDomeViewSet(viewsets.ModelViewSet):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    queryset = ShowSession.objects.all()
+    serializer_class = ShowSessionSerializer
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
