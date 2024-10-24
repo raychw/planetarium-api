2faaf29b25f3c6792775f552bce9dcfd98440e0d
reichikku <bladeryz@gmail.com>
2024-10-24 17:08:42 +0300

    Added a __str__ method for each model

--- planetarium/models.py
+++ planetarium/models.py
@@ -6,9 +6,15 @@ class AstronomyShow(models.Model):
     title = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.title
+
 
 class ShowTheme(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class PlanetariumDome(models.Model):
@@ -16,16 +22,25 @@ class PlanetariumDome(models.Model):
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class ShowSession(models.Model):
     astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
     planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
     show_time = models.DateTimeField()
 
+    def __str__(self):
+        return self.astronomy_show.title + "" + str(self.show_time)
+
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.created_at)
 
 
 class Ticket(models.Model):
@@ -33,3 +48,8 @@ class Ticket(models.Model):
     seat = models.IntegerField()
     show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (
+            f"{str(self.show_session)} (row: {self.row}, seat: {self.seat})"
+        )
