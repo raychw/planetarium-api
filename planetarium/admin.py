b54406ed467cd5d61414ea1a3c7958fc7bc32ba7
reichikku <bladeryz@gmail.com>
2024-10-25 16:20:47 +0300

    Registered models in admin.py, created a Ticket inline

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from planetarium.models import (
+    AstronomyShow,
+    PlanetariumDome,
+    Reservation,
+    ShowSession,
+    ShowTheme,
+    Ticket,
+)
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
+admin.site.register(AstronomyShow)
+admin.site.register(PlanetariumDome)
+admin.site.register(ShowSession)
+admin.site.register(ShowTheme)
+admin.site.register(Ticket)
--- planetarium/models.py
+++ planetarium/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class ShowTheme(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
