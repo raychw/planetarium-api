e9162dc751014079c7fe5a3cecb2619224f524f5
reichikku <bladeryz@gmail.com>
2024-10-30 12:58:31 +0200

    Fixed how token authentication worked

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -129,9 +129,12 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASS":
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-    "DEFAULT_PERMISSION_CLASS": "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10
 }
