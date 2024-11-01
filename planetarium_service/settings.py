5607f87a0c6424912e435e8c7d4637000a23ea16
reichikku <bladeryz@gmail.com>
2024-11-01 22:04:48 +0200

    Added throttling

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -140,5 +140,13 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day"
+    },
     "PAGE_SIZE": 10
 }
