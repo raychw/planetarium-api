785061d7d355a9e6ca55e9b509048716c180f5e7
reichikku <bladeryz@gmail.com>
2024-11-02 17:29:07 +0200

    Added django-debug-toolbar

--- planetarium_service/urls.py
+++ planetarium_service/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
     path("api/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
