a166d5702062de2e618354861e91d0ac29291a5a
reichikku <bladeryz@gmail.com>
2024-11-03 19:00:33 +0200

    Added env.sample

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1:8000"]
+ALLOWED_HOSTS = ["127.0.0.1:8000", "localhost"]
 
 
 # Application definition
